a6b3c7b9cbe9a35f5f7692ca0c0ac321f8fb9bf6
Alan Kasis <alankasis@gmail.com>
2024-05-15 11:41:16 -0300

    fix: header onclick

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,15 +19,15 @@ const Header = () => {
   }
 
   return (
-    <div className="relative md:text-xl w-full h-[40px] flex justify-between items-center p-4 py-10 md:p-10 absolute top-0 z-40" onClick={handleNavbarToggle}>
-      <div className="hamburger-icon" id="icon">
+    <div className="relative md:text-xl w-full h-[40px] flex justify-between items-center p-4 py-10 md:p-10 absolute top-0 z-40">
+      <div className="hamburger-icon" id="icon" onClick={handleNavbarToggle}>
         <div className={`icon-1 ${navbar ? "a" : ""}`} id="a"></div>
         <div className={`icon-2 ${navbar ? "c" : ""}`} id="b"></div>
         <div className={`icon-3 ${navbar ? "b" : ""}`} id="c"></div>
         <div className="clear"></div>
       </div>
       <Link href="/">Alan Kasis</Link>
-      <Link href="contact">Contact</Link>
+      <Link href="/contact">Contact</Link>
       {navbar && (
         <ul className="absolute top-0 translate-y-1/4 z-20">
           <AnimatedLink content="Home" href="/" />
